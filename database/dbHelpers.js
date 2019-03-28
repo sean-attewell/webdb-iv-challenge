19d52d716be37ee10bdfdc44fa575115d6910777
sean-attewell <sean.attewell@gmail.com>
2019-03-28 18:55:44 +0000

    MVP

--- database/dbHelpers.js
+++ database/dbHelpers.js
@@ -15,11 +15,15 @@ function getDishes() {
     return db("dishes");
 }
 
+// I think this fails where there's nothing to innerJoin to
+// aka looking up a new dish without any recipes
+// leftJoin fixes this problem and shows the recipie name as null
 function getDish(id) {
     return db("dishes")
-        .join("recipes", "dishes.id", "=", "recipes.dish_id")
-        .select("dishes.id", "dishes.dish_name", { recipe: "recipes.recipe_name" })
+        .leftJoin("recipes", "dishes.id", "=", "recipes.dish_id")
+        .select("dishes.id", "dishes.dish_name", "recipes.recipe_name")
         .where({ "dishes.id": id })
+        // .groupBy('dishes.dish_name')
 }
 
 function addDish(dish) {
@@ -27,13 +31,14 @@ function addDish(dish) {
         .insert(dish)
 }
 
+// there are dishes for all recipes so inner join has no beef
 function getRecipes() {
     return db("recipes")
         .join("dishes", "dishes.id", "=", "recipes.dish_id")
         .select(
-            { dish: "dishes.dish_name" },
             "recipes.id",
             "recipes.recipe_name",
+            { dish: "dishes.dish_name" },
             "recipes.instructions"
         );
 }
